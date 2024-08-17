f3f80771ab30182bfdcf7eb17202461bca93abdf
dhruvtongia <dhruvtongia2000@gmail.com>
2024-08-17 12:12:59 +0530

    project-build-service redis integration

--- project-build-service/index.js
+++ project-build-service/index.js
@@ -14,8 +14,8 @@ const git = simpleGit().clean(simpleGit.CleanOptions.FORCE);
 const s3Client = new S3Client({
   region: "eu-north-1",
   credentials: {
-    accessKeyId: "",
-    secretAccessKey: "",
+    accessKeyId: "AKIAW3MEDQL6WFTCH7O7",
+    secretAccessKey: "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",
   },
 });
 
